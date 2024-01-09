64aefb1f4ed36665ab654824bdd8e2ff8939b70b
sansk <sansk@KOIRALASANSKRIT>
2024-01-09 07:57:36 +0545

    Kachra Removed and Some better things added

--- jkrgui/jkrgui.lua
+++ jkrgui/jkrgui.lua
@@ -1,28 +1,23 @@
 --[[
 JkrGUI lua
-]]
-
-
-local AlternativeTextRenderer = true
+]] local AlternativeTextRenderer = true
 
 -- this is for utf8 strings (the string that supports कखग), ignore it
 
 function utf8.sub(s, i, j)
-        i = utf8.offset(s, i)
-        j = utf8.offset(s, j + 1) - 1
-        return string.sub(s, i, j)
+    i = utf8.offset(s, i)
+    j = utf8.offset(s, j + 1) - 1
+    return string.sub(s, i, j)
 end
 
 -- These are aliases (Same name for an identifier), the Lua Bindings that is generated has snake case (snake_case), trying to convert into
 -- CamelCase (words separated by Capital letters).
-
 
 --[[
 returns vec2 that gives window's width and height, local dimen = GetWindowDimensions()
 dimen.x ani dimen.y garna milxa
 ]]
 GetWindowDimensions = get_window_dimensions
-
 
 --[[
         returns identity matrix mat4 = {
@@ -104,7 +99,7 @@ local line_bound = false
         This is the shape renderer, we can do S.something
 ]]
 
-S = Jkr.sh        -- Shape Renderer
+S = Jkr.sh -- Shape Renderer
 --[[
         We can add shapes with S.Add()
 ]]
@@ -146,7 +141,6 @@ uvec2 = uvec2
 vec3 = vec3
 vec4 = vec4
 
-
 Key = key
 
 require "jkrgui.Config" -- #include "Config" vane jastai C ma
@@ -163,37 +157,35 @@ WindowDimension = GetWindowDimensions() -- Can get Window dimensions just by doi
 
 -- To be called at Update Callback
 function FrameUpdate()
-        Time = Time + 1
-        WindowDimension = GetWindowDimensions()
+    Time = Time + 1
+    WindowDimension = GetWindowDimensions()
 end
-
-
 
 --[[
         This is a fontObject, which holds a certain font of certain size
 ]]
 
 Jkr.FontObject = {
-        mPath = " ",
-        mId = 0,
-        mSize = 0,
-        New = function(self, inPath, inSize)
-                local Object = {
-                        mPath = " ",
-                        mId = 0,
-                        mSize = 0
-                }
-                setmetatable(Object, self)
-                self.__index = self                     -- Elle garne k ho vane Returned object bata Obj.GetDimension etc garna milxa
-                Object.mPath = inPath
-                Object.mSize = inSize
-                Object.mId = T.AddFontFace(Object.mPath, Object.mSize)
-                return Object
-        end,
-        -- returns vec2
-        GetDimension = function(self, inString)
-                return T.GetTextDimension(inString, Int(self.mId))
-        end
+    mPath = " ",
+    mId = 0,
+    mSize = 0,
+    New = function(self, inPath, inSize)
+        local Object = {
+            mPath = " ",
+            mId = 0,
+            mSize = 0
+        }
+        setmetatable(Object, self)
+        self.__index = self -- Elle garne k ho vane Returned object bata Obj.GetDimension etc garna milxa
+        Object.mPath = inPath
+        Object.mSize = inSize
+        Object.mId = T.AddFontFace(Object.mPath, Object.mSize)
+        return Object
+    end,
+    -- returns vec2
+    GetDimension = function(self, inString)
+        return T.GetTextDimension(inString, Int(self.mId))
+    end
 }
 
 --[[
@@ -202,58 +194,58 @@ Jkr.FontObject = {
 ]]
 
 Jkr.ComponentObject = {
-        mPosition_3f = vec3(0, 0, 0),
-        mDimension_3f = vec3(0, 0, 0),
-        mFocus_b = false,
-        mBoundedRectId_i = 0,
-        mFocusOnHover_b = false,
-        mTransparentToMouse_b = false,
-        New = function(self, inPosition_3f, inDimension_3f)
-                local Obj = {}
-                --("Component Construction")
-                setmetatable(Obj, self)
-                self.__index = self
+    mPosition_3f = vec3(0, 0, 0),
+    mDimension_3f = vec3(0, 0, 0),
+    mFocus_b = false,
+    mBoundedRectId_i = 0,
+    mFocusOnHover_b = false,
+    mTransparentToMouse_b = false,
+    New = function(self, inPosition_3f, inDimension_3f)
+        local Obj = {}
+        -- ("Component Construction")
+        setmetatable(Obj, self)
+        self.__index = self
 
-                Obj.mPosition_3f = inPosition_3f
-                Obj.mDimension_3f = inDimension_3f
-                Obj.mFocus_b = false
-                Obj.mFocusOnHover_b = true
-                Obj.mTransparentToMouse_b = false
-                local pos = vec2(Obj.mPosition_3f.x, Obj.mPosition_3f.y)
-                local dim = vec2(Obj.mDimension_3f.x, Obj.mDimension_3f.y)
-                Obj.mBoundedRectId_i = E.set_bounded_rect(pos, dim, Int(Obj.mPosition_3f.z))
-                --("Component Construction Finished")
-                return Obj
-        end,
+        Obj.mPosition_3f = inPosition_3f
+        Obj.mDimension_3f = inDimension_3f
+        Obj.mFocus_b = false
+        Obj.mFocusOnHover_b = true
+        Obj.mTransparentToMouse_b = false
+        local pos = vec2(Obj.mPosition_3f.x, Obj.mPosition_3f.y)
+        local dim = vec2(Obj.mDimension_3f.x, Obj.mDimension_3f.y)
+        Obj.mBoundedRectId_i = E.set_bounded_rect(pos, dim, Int(Obj.mPosition_3f.z))
+        -- ("Component Construction Finished")
+        return Obj
+    end,
 
-        -- This is function that has to run each frame, OR on demand
-        Update = function(self, inPosition_3f, inDimension_3f)
-                self.mPosition_3f = inPosition_3f
-                self.mDimension_3f = inDimension_3f
-                local pos = vec2(self.mPosition_3f.x, self.mPosition_3f.y)
-                local dim = vec2(self.mDimension_3f.x, self.mDimension_3f.y)
-                E.update_bounded_rect(Int(self.mBoundedRectId_i), pos, dim, Int(self.mPosition_3f.z))
-        end,
+    -- This is function that has to run each frame, OR on demand
+    Update = function(self, inPosition_3f, inDimension_3f)
+        self.mPosition_3f = inPosition_3f
+        self.mDimension_3f = inDimension_3f
+        local pos = vec2(self.mPosition_3f.x, self.mPosition_3f.y)
+        local dim = vec2(self.mDimension_3f.x, self.mDimension_3f.y)
+        E.update_bounded_rect(Int(self.mBoundedRectId_i), pos, dim, Int(self.mPosition_3f.z))
+    end,
 
-        -- Mouse le maathi gayo vane focus hune nahune
-        -- Jastai, mTransparentToMouse_b = true vayo vane, kunai element ko muntira vae pani focus hunxa
-        Event = function(self)
-                if self.mFocusOnHover_b or E.is_left_button_pressed() then
-                        if self.mTransparentToMouse_b then
-                                self.mFocus_b = E.is_mouse_within(Int(self.mBoundedRectId_i), Int(self.mPosition_3f.z))
-                        else
-                                self.mFocus_b = E.is_mouse_on_top(Int(self.mBoundedRectId_i), Int(self.mPosition_3f.z))
-                        end
-                else
-                        self.mFocus_b = false
-                end
-        end,
-
-        GetTranslationMatrix = function(self)
-                local matrix = GetIdentityMatrix()
-                local matrix = Translate(matrix, vec3(self.mPosition.x, self.mPosition.y, 0))
-                return matrix
+    -- Mouse le maathi gayo vane focus hune nahune
+    -- Jastai, mTransparentToMouse_b = true vayo vane, kunai element ko muntira vae pani focus hunxa
+    Event = function(self)
+        if self.mFocusOnHover_b or E.is_left_button_pressed() then
+            if self.mTransparentToMouse_b then
+                self.mFocus_b = E.is_mouse_within(Int(self.mBoundedRectId_i), Int(self.mPosition_3f.z))
+            else
+                self.mFocus_b = E.is_mouse_on_top(Int(self.mBoundedRectId_i), Int(self.mPosition_3f.z))
+            end
+        else
+            self.mFocus_b = false
         end
+    end,
+
+    GetTranslationMatrix = function(self)
+        local matrix = GetIdentityMatrix()
+        local matrix = Translate(matrix, vec3(self.mPosition.x, self.mPosition.y, 0))
+        return matrix
+    end
 }
 
 Jkr.Components = {}
@@ -261,149 +253,148 @@ Jkr.Components.Static = {}
 Jkr.Components.Abstract = {}
 Jkr.Components.Util = {}
 
-
 Jkr.Components.Abstract.ImageObject = {
-        mId = nil,
-        New = function (self, inWidth, inHeight, inFileName)
-               local Obj = {}
-               setmetatable(Obj, self)
-               self.__index = self
-               if inFileName then
-                        Obj.mId = S.AddImage(inFileName)
-               else
-                        Obj.mId = S.AddImage(Int(inWidth), Int(inHeight))
-               end 
-               return Obj
+    mId = nil,
+    New = function(self, inWidth, inHeight, inFileName)
+        local Obj = {}
+        setmetatable(Obj, self)
+        self.__index = self
+        if inFileName then
+            Obj.mId = S.AddImage(inFileName)
+        else
+            Obj.mId = S.AddImage(Int(inWidth), Int(inHeight))
         end
+        return Obj
+    end
 }
 
-
 Jkr.Components.Static.ShapeObject = {
-        mScissorPosition_2f = nil,
-        mScissorDimension_2f = nil,
-        mComponentObject = nil,
-        mShapeId = nil,
-        mImageId = nil,
-        mFillColor = nil,
-        New = function(self, inPosition_3f, inDimension_3f, inImageObject)
-                local Obj = {
-                        mScissorPosition_2f = nil,
-                        mScissorDimension_2f = nil,
-                        mComponentObject = nil,
-                        mShapeId = nil,
-                        mImageId = nil,
-                        mFillColor = nil
-                }
+    mScissorPosition_2f = nil,
+    mScissorDimension_2f = nil,
+    mComponentObject = nil,
+    mShapeId = nil,
+    mImageId = nil,
+    mFillColor = nil,
+    New = function(self, inPosition_3f, inDimension_3f, inImageObject)
+        local Obj = {
+            mScissorPosition_2f = nil,
+            mScissorDimension_2f = nil,
+            mComponentObject = nil,
+            mShapeId = nil,
+            mImageId = nil,
+            mFillColor = nil
+        }
 
-                setmetatable(Obj, self)
-                self.__index = self
+        setmetatable(Obj, self)
+        self.__index = self
 
-                Obj.mComponentObject = Jkr.ComponentObject:New(inPosition_3f, inDimension_3f)
-                local Dimension = vec2(Obj.mComponentObject.mDimension_3f.x, Obj.mComponentObject.mDimension_3f.y)
-                local rect_gen = Generator(Shapes.rectangle, Dimension)
-                Obj.mShapeId =  S.Add(rect_gen, Obj.mComponentObject.mPosition_3f)
+        Obj.mComponentObject = Jkr.ComponentObject:New(inPosition_3f, inDimension_3f)
+        local Dimension = vec2(Obj.mComponentObject.mDimension_3f.x, Obj.mComponentObject.mDimension_3f.y)
+        local rect_gen = Generator(Shapes.rectangle, Dimension)
+        Obj.mShapeId = S.Add(rect_gen, Obj.mComponentObject.mPosition_3f)
 
-                if inImageObject then
-                        Obj.mImageId = inImageObject.mId
-                else
-                        Obj.mImageId = nil
-                end
-
-                Obj.mFillColor = vec4(1, 1, 1, 1)
-                return Obj
-        end,
-        Update = function (self, inPosition_3f, inDimension_3f)
-               local Dimension = vec2(inDimension_3f.x, inDimension_3f.y)
-               local rect_gen = Generator(Shapes.rectangle, Dimension)
-               S.Update(Int(self.mShapeId), rect_gen, inPosition_3f) 
-               self.mComponentObject:Update(inPosition_3f, inDimension_3f)
-        end,
-        Event = function(self)
-                self.mComponentObject:Event()
-        end,
-        Draw = function(self)
-                S.Bind()
-                if self.mImageId then
-                        S.BindFillMode(FillType.image)
-                        S.BindImage(self.mImageId)
-                else
-                        S.BindFillMode(FillType.fill)
-                end
-                S.Draw(self.mFillColor, Int(WindowDimension.x), Int(WindowDimension.y), Int(self.mShapeId), Int(self.mShapeId), GetIdentityMatrix())
+        if inImageObject then
+            Obj.mImageId = inImageObject.mId
+        else
+            Obj.mImageId = nil
         end
+
+        Obj.mFillColor = vec4(1, 1, 1, 1)
+        return Obj
+    end,
+    Update = function(self, inPosition_3f, inDimension_3f)
+        local Dimension = vec2(inDimension_3f.x, inDimension_3f.y)
+        local rect_gen = Generator(Shapes.rectangle, Dimension)
+        S.Update(Int(self.mShapeId), rect_gen, inPosition_3f)
+        self.mComponentObject:Update(inPosition_3f, inDimension_3f)
+    end,
+    Event = function(self)
+        self.mComponentObject:Event()
+    end,
+    Draw = function(self)
+        S.Bind()
+        if self.mImageId then
+            S.BindFillMode(FillType.image)
+            S.BindImage(self.mImageId)
+        else
+            S.BindFillMode(FillType.fill)
+        end
+        S.Draw(self.mFillColor, Int(WindowDimension.x), Int(WindowDimension.y), Int(self.mShapeId), Int(self.mShapeId),
+            GetIdentityMatrix())
+    end
 }
 
 Jkr.Components.Static.TextObject = {
-        mScissorPosition_2f = nil,
-        mScissorDimension_2f = nil,
-        mString = nil,
-        mFont = nil, -- Font Object
-        mId = nil,  
-        mDimension_2f = nil,
-        mColor = Theme.Colors.Text.Normal,
-        New = function (self, inText, inPosition_3f, inFontObject)
-                local Obj = {
-                        mScissorPosition_2f = nil,
-                        mScissorDimension_2f = nil,
-                        mString = nil,
-                        mFont = nil, -- Font Object
-                        mId = nil,  
-                        mDimension_2f = nil,
-                        mColor = Theme.Colors.Text.Normal,
-                        mAlt = AlternativeTextRenderer
-                }
+    mScissorPosition_2f = nil,
+    mScissorDimension_2f = nil,
+    mString = nil,
+    mFont = nil, -- Font Object
+    mId = nil,
+    mDimension_2f = nil,
+    mColor = Theme.Colors.Text.Normal,
+    New = function(self, inText, inPosition_3f, inFontObject)
+        local Obj = {
+            mScissorPosition_2f = nil,
+            mScissorDimension_2f = nil,
+            mString = nil,
+            mFont = nil, -- Font Object
+            mId = nil,
+            mDimension_2f = nil,
+            mColor = Theme.Colors.Text.Normal,
+            mAlt = AlternativeTextRenderer
+        }
 
-                setmetatable(Obj, self)
-                self.__index = self
+        setmetatable(Obj, self)
+        self.__index = self
 
-                Obj.mString = inText
-                Obj.mPosition_3f = inPosition_3f
+        Obj.mString = inText
+        Obj.mPosition_3f = inPosition_3f
 
-                if not Obj.mAlt then
-                        T.SetCurrentFace(inFontObject.mId)
-                        T.SetTextProperty(TextH.left, TextV.top)
-                        Obj.mId = T.Add(Obj.mString, vec3(Obj.mPosition_3f.x, Obj.mPosition_3f.y, Depth))
-                        Obj.mDimension_2f = inFontObject:GetDimension(Obj.mString)
-                else
-                        Obj.mId = r.balt.add(inFontObject.mId, Obj.mString, vec3(Obj.mPosition_3f.x, Obj.mPosition_3f.y, Depth))
-                end
-
-                Obj.mFont = inFontObject
-
-                return Obj
-        end,
-        GetLength = function(self)
-                return self.mId.y
-        end,
-        Event = function (self)
-        end,
-        Draw = function (self)
-
-                if not self.mAlt then
-                        T.Bind()
-                        T.Draw(self.mColor, Int(WindowDimension.x), Int(WindowDimension.y), Int(self.mId.x), Int(self.mId.y), GetIdentityMatrix())
-                else
-                        S.Bind()
-                        S.BindFillMode(FillType.image)
-                        r.balt.draw(self.mColor, Int(WindowDimension.x), Int(WindowDimension.y), self.mId, GetIdentityMatrix())
-                end
-        end,
-        Update = function (self, inPosition_3f)
-                self.mPosition_3f = inPosition_3f
-                if not self.mAlt then
-                        local str = string.rep(" ", self.mId.y)
-                        T.Update(Int(self.mId.x), str, self.mPosition_3f)
-                        T.Update(Int(self.mId.x), self.mString, self.mPosition_3f)
-                else
-                        r.balt.update(self.mId, Int(self.mFont.mId), self.mPosition_3f, self.mString)
-                end
-        end,
-        -- TODO Remove this function
-        SetScissor = function (self)
-                if self.mScissorPosition_2f and self.mScissorDimension_2f then
-                        Jkr.set_scissor(self.mScissorPosition_2f, self.mScissorDimension_2f)
-                end
+        if not Obj.mAlt then
+            T.SetCurrentFace(inFontObject.mId)
+            T.SetTextProperty(TextH.left, TextV.top)
+            Obj.mId = T.Add(Obj.mString, vec3(Obj.mPosition_3f.x, Obj.mPosition_3f.y, Depth))
+            Obj.mDimension_2f = inFontObject:GetDimension(Obj.mString)
+        else
+            Obj.mId = r.balt.add(inFontObject.mId, Obj.mString, vec3(Obj.mPosition_3f.x, Obj.mPosition_3f.y, Depth))
         end
+
+        Obj.mFont = inFontObject
+
+        return Obj
+    end,
+    GetLength = function(self)
+        return self.mId.y
+    end,
+    Event = function(self)
+    end,
+    Draw = function(self)
+        if not self.mAlt then
+            T.Bind()
+            T.Draw(self.mColor, Int(WindowDimension.x), Int(WindowDimension.y), Int(self.mId.x), Int(self.mId.y),
+                GetIdentityMatrix())
+        else
+            S.Bind()
+            S.BindFillMode(FillType.image)
+            r.balt.draw(self.mColor, Int(WindowDimension.x), Int(WindowDimension.y), self.mId, GetIdentityMatrix())
+        end
+    end,
+    Update = function(self, inPosition_3f)
+        self.mPosition_3f = inPosition_3f
+        if not self.mAlt then
+            local str = string.rep(" ", self.mId.y)
+            T.Update(Int(self.mId.x), str, self.mPosition_3f)
+            T.Update(Int(self.mId.x), self.mString, self.mPosition_3f)
+        else
+            r.balt.update(self.mId, Int(self.mFont.mId), self.mPosition_3f, self.mString)
+        end
+    end,
+    -- TODO Remove this function
+    SetScissor = function(self)
+        if self.mScissorPosition_2f and self.mScissorDimension_2f then
+            Jkr.set_scissor(self.mScissorPosition_2f, self.mScissorDimension_2f)
+        end
+    end
 }
 
 Jkr.Components.Util.ImagePainter = {
@@ -422,8 +413,8 @@ Jkr.Components.Util.ImagePainter = {
         Obj.mPainter:register_image(Int(inImageSize_2f.x), Int(inImageSize_2f.y))
         return Obj
     end,
-    NewFrom = function (self, inPainter, inFileName, inStore_b, inGLSL, inX, inY, inZ)
-        local Obj = {} 
+    NewFrom = function(self, inPainter, inFileName, inStore_b, inGLSL, inX, inY, inZ)
+        local Obj = {}
         setmetatable(Obj, self)
         self.__index = self
         Obj.mPainter = inPainter.mPainter:make_from(inFileName, inGLSL)
@@ -435,8 +426,17 @@ Jkr.Components.Util.ImagePainter = {
         Obj.mPainter:register_image_existing()
         return Obj
     end,
-    Paint = function (self, inPosDimen_4f, inColor_4f, inParam_4f, inImageObject)
+    Paint = function(self, inPosDimen_4f, inColor_4f, inParam_4f, inImageObject)
         self.mPainter:paint(inPosDimen_4f, inColor_4f, inParam_4f)
-        S.CopyImage(Int(inImageObject.mId), self.mPainter)
+        if (inImageObject) then
+            S.CopyImage(Int(inImageObject.mId), self.mPainter)
+        end
+    end,
+    PaintImages = function(self, inPosDimen_4f, inColor_4f, inParam_4f, ...)
+        self.mPainter:paint(inPosDimen_4f, inColor_4f, inParam_4f)
+        local args = table.pack(...)
+        for i, v in ipairs(args) do
+            S.CopyImage(Int(v.mId), self.mPainter)
+        end
     end
 }
