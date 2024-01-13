17d22137fea2fc74efa794355ce262f2f68b4089
Bishal Kumar Jaiswal <077bct022.bishal@pcampus.edu.np>
2024-01-13 14:57:16 +0545

    MaterialComponent file made

--- jkrgui/ExtraComponents.lua
+++ jkrgui/ExtraComponents.lua
@@ -353,72 +353,7 @@ Com.ImgRect = {
         S.Update(Int(self.mId), rect_gen, self.mPosition_3f)
     end
 }
-Com.CheckButton = {
-    New = function(self, inTableOfString, inFontObject, inPadding, inLengthCellDimension)
-        local Obj = {
-            mTableOfString = inTableOfString,
-            mTableObjectForDescription = {},
-            mButtonChecked = {},
-            mButtonUnchecked = {},
-            mPadding = inPadding,
-            mLengthCellDimension = inLengthCellDimension,
-            mFirst = true,
-            mIndex = nil,
-        }
 
-        setmetatable(Obj, self)
-        self.__index = self
-        Obj.mPosition_3f = {}
-        Obj.mDimension_3f = {}
-        Obj.NoOfObject = #inTableOfString
-        for index, value in ipairs(inTableOfString) do
-            Obj.mTableObjectForDescription[index] = Com.TextButtonObject:New(inTableOfString[index].name,
-                inFontObject, vec3(0, 0, 80), vec3(0, 0, 0))
-            Obj.mButtonChecked[index] = Com.ImageLabelObject:New("icons_material/radio_button_checked/baseline-2x.png",
-                vec3(0, 0, 0), vec3(0, 0, 0))
-            Obj.mButtonChecked[index]:TintColor(vec4(0, 0, 1, 1))
-            Obj.mButtonUnchecked[index] = Com.ImageLabelObject:New(
-                "icons_material/radio_button_unchecked/baseline-2x.png",
-                vec3(0, 0, 0), vec3(0, 0, 0))
-            Obj.mButtonUnchecked[index]:TintColor(vec4(0, 0, 1, 1))
-        end
-        return Obj
-    end,
-    Update = function(self, inPosition_3f, inDimension_3f)
-        local position = vec3(inPosition_3f.x, inPosition_3f.y, inPosition_3f.z)
-        for i = 1, self.NoOfObject, 1 do
-            if self.mIndex == i or self.mIndex == nil then
-                if self.mFirst then
-                    self.mButtonUnchecked[i]:Update(vec3(position.x, position.y, position.z), inDimension_3f)
-                    self.mButtonChecked[i]:Update(vec3(0, 0, 0), vec3(0, 0, 0))
-                else
-                    self.mButtonChecked[i]:Update(vec3(position.x, position.y, position.z), inDimension_3f)
-                    self.mButtonUnchecked[i]:Update(vec3(0, 0, 0), vec3(0, 0, 0))
-                end
-            end
-            self.mPosition_3f[i] = vec3(position.x, position.y, position.z)
-            self.mDimension_3f[i] = vec3(inDimension_3f.x, inDimension_3f.y, inDimension_3f.z)
-            position.y = position.y + inDimension_3f.y + self.mPadding
-        end
-        for i = 1, self.NoOfObject, 1 do
-            self.mTableObjectForDescription[i]:Update(vec3(self.mPosition_3f[i].x+inDimension_3f.x+self.mPadding,self.mPosition_3f[i].y,self.mPosition_3f[i].z),vec3(self.mLengthCellDimension,inDimension_3f.y,inDimension_3f.z),self.mTableObjectForDescription[i].mText) 
-        end
-    end,
-    Event = function(self)
-        local MousePos = E.get_mouse_pos()
-        for i = 1, self.NoOfObject, 1 do
-            if E.is_left_button_pressed() then
-                
-                if MousePos.x > self.mPosition_3f[i].x and MousePos.x < (self.mPosition_3f[i].x + self.mDimension_3f[i].x) and MousePos.y > self.mPosition_3f[i].y and MousePos.y < (self.mPosition_3f[i].y + self.mDimension_3f[i].y) then
-                    self.mFirst = not self.mFirst
-                    self.mIndex = i
-                    self:Update(self.mPosition_3f[1], self.mDimension_3f[1]) --first button ko position dinuprxaw
-                end
-            end
-        end
-    end
-
-}
 Com.ToggleButton = {
     New = function(self, inPosition_3f, inDimension_3f)
         local Obj = {}
@@ -459,5 +394,21 @@ Com.ToggleButton = {
             self.mTableForObject[1]:TintColor(vec4(0, 0, 1, 1))
             self.mTableForObject[2]:TintColor(vec4(0, 0, 0, 0.5))
         end
+    end
+}
+Com.ComboBox = {
+    New = function(self, inComboContent)
+        local Obj = {
+
+        }
+        setmetatable(Obj, self)
+        self.__index = self
+        return Obj
+    end,
+    Update = function(self, inPosition_3f, inDimension_3f)
+
+    end,
+    Event = function(self)
+
     end
 }
