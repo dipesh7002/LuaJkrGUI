ca19cb2c6147d19d822bdb006e98b2c26c3329fe
Bishal Kumar Jaiswal <077bct022.bishal@pcampus.edu.np>
2024-01-19 16:46:45 +0545

    Chaliraxaina window layout

--- jkrgui/LayoutComponents.lua
+++ jkrgui/LayoutComponents.lua
@@ -4,6 +4,8 @@ Com.HLayout = {
     mComponents = nil,
     mRatioTable = nil,
     mPadding = nil,
+    mPosition_3f = nil,
+    mDimension_3f = nil,
 
     New = function(self, inPadding)
         local Obj = {
@@ -19,8 +21,10 @@ Com.HLayout = {
         self.mRatioTable = inRatioTable
     end,
     Update = function(self, inPosition_3f, inDimension_3f)
-        local position = inPosition_3f
-        local dimension = inDimension_3f
+        local position = vec3(inPosition_3f.x, inPosition_3f.y, inPosition_3f.z)
+        local dimension = vec3(inDimension_3f.x, inDimension_3f.y, inDimension_3f.z)
+        self.mPosition_3f = inPosition_3f
+        self.mDimension_3f = inDimension_3f
         local paddingX = self.mPadding
 
         if self.mRatioTable then
@@ -31,6 +35,16 @@ Com.HLayout = {
                 position.x = position.x + dimension.x * self.mRatioTable[index] + paddingX
             end
         end
+    end,
+    GetComponentPosition = function(self)
+        local position = vec3(self.mPosition_3f.x, self.mPosition_3f.y, self.mPosition_3f.z)
+        local dimension = vec3(self.mDimension_3f.x, self.mDimension_3f.y, self.mDimension_3f.z)
+        local ComponentsPosition = {}
+        for index, value in ipairs(self.mComponents) do
+            ComponentsPosition[index] = vec3(position.x, position.y, position.z)
+            position.x = position.x + dimension.x * self.mRatioTable[index] + self.mPadding
+        end
+        return ComponentsPosition
     end
 }
 
@@ -38,6 +52,8 @@ Com.VLayout = {
     mComponents = nil,
     mRatioTable = nil,
     mPadding = nil,
+    mPosition_3f = nil,
+    mDimension_3f = nil,
 
     New = function(self, inPadding)
         local Obj = {
@@ -52,9 +68,11 @@ Com.VLayout = {
         self.mRatioTable = inRatioTable
     end,
     Update = function(self, inPosition_3f, inDimension_3f)
-        local position = vec3(inPosition_3f.x ,inPosition_3f.y, inPosition_3f.z)
-        local dimension = vec3(inDimension_3f.x,inDimension_3f.y,inDimension_3f.z)
-        local paddingY = self.mPadding 
+        local position = vec3(inPosition_3f.x, inPosition_3f.y, inPosition_3f.z)
+        local dimension = vec3(inDimension_3f.x, inDimension_3f.y, inDimension_3f.z)
+        self.mPosition_3f = inPosition_3f
+        self.mDimension_3f = inDimension_3f
+        local paddingY = self.mPadding
         if self.mRatioTable then
             for index, value in ipairs(self.mComponents) do
                 value:Update(vec3(position.x, position.y, position.z),
@@ -63,6 +81,16 @@ Com.VLayout = {
                 position.y = position.y + dimension.y * self.mRatioTable[index] + paddingY
             end
         end
+    end,
+    GetComponentPosition = function(self)
+        local position = vec3(self.mPosition_3f.x, self.mPosition_3f.y, self.mPosition_3f.z)
+        local dimension = vec3(self.mDimension_3f.x, self.mDimension_3f.y, self.mDimension_3f.z)
+        local ComponentsPosition = {}
+        for index, value in ipairs(self.mComponents) do
+            ComponentsPosition[index] = vec3(position.x, position.y, position.z)
+            position.y = position.y + dimension.y * self.mRatioTable[index] + self.mPadding
+        end
+        return ComponentsPosition
     end
 }
 
@@ -72,7 +100,7 @@ Com.VLayout = {
     Esko euta CentralComponent Object hunxa,
     tyo mComponentObject vaneko chae Jkr.ComponentObject ho, gaera hernu tyaa k xa vanera
     mainly, tei event ko laagi ho mouse maathi aauda focus garne na garne, left button thichda focus hune na hune
-    ani Z value anusaar kun chae Component(Jkr.ComponentObject) wala lai select garne jasto kura haru handle garxa 
+    ani Z value anusaar kun chae Component(Jkr.ComponentObject) wala lai select garne jasto kura haru handle garxa
 
     ani yo use esari garne ho
 
@@ -100,7 +128,7 @@ Com.VLayout = {
 Com.WindowLayout = {
     mHitArea_2f = nil,
     mComponentObject = nil, -- yo hamro wala components haina, Jkr.ComponentObject wala component ho, esko naam fernu parlaa jasto xa TODO,
-                            -- maintainence ko bela garnu parxa
+    -- maintainence ko bela garnu parxa
     mCentralComponent = nil,
     mPosition_3f = nil,
     mDimension_3f = nil,
@@ -111,7 +139,6 @@ Com.WindowLayout = {
         self.__index = self
         Obj.mHitArea_2f = inHitArea_2f
         Obj.mComponentObject = Jkr.ComponentObject:New(inPosition_3f, vec3(inHitArea_2f.x, inHitArea_2f.y, 1))
-        Obj.mComponentObject.mFocusOnHover_b = false
         Obj.mPosition_3f = inPosition_3f
         Obj.mDimension_3f = inDimension_3f
         Obj.mMoving = false
@@ -120,16 +147,18 @@ Com.WindowLayout = {
     Start = function(self)
         Com.NewComponent()
         local i = com_i
-        ComTable[com_i] = Jkr.Components.Abstract.Drawable:New(function ()
+        ComTable[com_i] = Jkr.Components.Abstract.Drawable:New(function()
             local offset = vec2(self.mPosition_3f.x, self.mPosition_3f.y)
             local extent = vec2(self.mDimension_3f.x, self.mDimension_3f.y)
-            Jkr.set_scissor(offset, extent)
+            if offset.x > 0 and offset.y > 0 then
+                Jkr.set_scissor(offset, extent)
+            end
         end)
     end,
     End = function(self)
         Com.NewComponent()
         local i = com_i
-        ComTable[com_i] = Jkr.Components.Abstract.Drawable:New(function ()
+        ComTable[com_i] = Jkr.Components.Abstract.Drawable:New(function()
             Jkr.reset_scissor()
         end)
     end,
@@ -140,7 +169,7 @@ Com.WindowLayout = {
         ComTable_Event[com_evi] = Jkr.Components.Abstract.Eventable:New(
             function()
                 self.mComponentObject:Event()
-                if self.mComponentObject.mFocus_b or (self.mMoving and E.is_left_button_pressed()) then
+                if self.mComponentObject.mClicked_b or self.mMoving then
                     local mpos = E.get_relative_mouse_pos()
                     self.mPosition_3f.x = self.mPosition_3f.x + mpos.x
                     self.mPosition_3f.y = self.mPosition_3f.y + mpos.y
@@ -151,9 +180,58 @@ Com.WindowLayout = {
                     self.mMoving = false
                 end
             end
-         )
+        )
     end,
     Update = function(self, inPosition_3f, inDimension_3f)
         self.mCentralComponent:Update(inPosition_3f, inDimension_3f)
+    end
+}
+
+
+Com.ButtonProxy = {
+    mComponentObject = nil,
+    mPosition_3f = nil,
+    mDimension_3f = nil,
+    mHoverFunction = nil,
+    mClickedFunction = nil,
+    mClickedOutfunction = nil,
+
+    New = function(self, inPosition_3f, inDimension_3f)
+        local Obj = {
+            mComponentObject = Jkr.ComponentObject:New(inPosition_3f, inDimension_3f),
+            mPosition_3f = inPosition_3f,
+            mDimension_3f = inDimension_3f,
+            mHoverFunction = function() end,
+            mClickedFunction = function() end,
+            mClickedOutfunction = function() end,
+        }
+        setmetatable(Obj, self)
+        self.__index = self
+        Com.NewComponent_Event()
+        ComTable_Event[com_evi] = Jkr.Components.Abstract.Eventable:New(
+            function()
+                Obj.mComponentObject:Event()
+                if Obj.mComponentObject.mHovered_b then
+                    Obj.mHoverFunction()
+                end
+
+                if Obj.mComponentObject.mClicked_b then
+                    Obj.mClickedFunction()
+                end
+                if Obj.mComponentObject.mClickedOut_b then
+                    Obj.mClickedOutfunction()
+                end
+            end
+        )
+        return Obj
+    end,
+    SetFunctions = function(self, inHoverFunction, inClickedFunction)
+        self.mHoverFunction = inHoverFunction
+        self.mClickedFunction = inClickedFunction
+    end,
+    Update = function(self, inPosition_3f, inDimension_3f)
+        self.mPosition_3f = inPosition_3f
+        self.mDimension_3f = inDimension_3f
+        self.mComponentObject:Update(inPosition_3f, inDimension_3f)
     end
 }
