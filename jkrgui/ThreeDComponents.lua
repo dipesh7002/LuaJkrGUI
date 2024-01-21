29c95d2ce7925f7bbb06a62253d9bb0513b05d2a
Darshan Koirala <theanimatorspal@gmail.com>
2024-01-21 16:16:20 +0545

    Fuck This FUck

--- jkrgui/ThreeDComponents.lua
+++ jkrgui/ThreeDComponents.lua
@@ -1,6 +1,12 @@
 require "jkrgui.jkrgui"
+Jmath3D = jmath3D
 
 Com.Objec3D = {
-    New = function()
+    New = function(self)
+        local Obj = {}
+        setmetatable(Obj, self)
+        self.__index = self
+
+        return Obj
     end
 }
