4345709c11100c4f340a9c416f3444f1d584dae8
Darshan Koirala <theanimatorspal@gmail.com>
2024-01-23 07:48:43 +0545

    Cursor At Correct Position

--- jkrgui/TextEditorComponents.lua
+++ jkrgui/TextEditorComponents.lua
@@ -23,6 +23,7 @@ Com.TextCursorObject = {
 
 Com.TextLineObject = {
     mTextObjectId = nil,
+    mPositionInMultiline = nil,
     New = function(self, inPosition_3f, inDimension_3f, inMaxChars, inFontObject)
         local Obj = {}
         setmetatable(Obj, self)
@@ -32,8 +33,9 @@ Com.TextLineObject = {
         Obj.mTextObjectId = com_i
         return Obj
     end,
-    Update = function(self, inPosition_3f, inDimension_3f, inString)
+    Update = function(self, inPosition_3f, inDimension_3f, inString, inPositionInMultiline)
         ComTable[self.mTextObjectId]:Update(inPosition_3f, inDimension_3f, inString)
+        self.mPositionInMultiline = inPositionInMultiline
     end
 }
 
@@ -44,8 +46,7 @@ Com.TextMultiLineObject = {
     mVerticalDrawSpacing = nil,
     mFontObject = nil,
     mShouldWrap = false,
-    New = function(self, inPosition_3f, inDimension_3f, inFontObject, inMaxNoOfLines, inMaxChars, inVerticalDrawSpacing,
-                   inShouldWrap)
+    New = function(self, inPosition_3f, inDimension_3f, inFontObject, inMaxNoOfLines, inMaxChars, inVerticalDrawSpacing, inShouldWrap)
         local Obj = {}
         setmetatable(Obj, self)
         self.__index = self
@@ -66,22 +67,40 @@ Com.TextMultiLineObject = {
         end
         return Obj
     end,
+    InsertAt = function (self, inPosition_3f, inDimension_3f, inLineNo, inCharacterIndex, inCharacter)
+        local str = self.mStringBuffer
+        local pos = self.mLineStringBuffers[inLineNo].mPositionInMultiline
+        --local newstr = 
+        --utf8.sub(str, )
+    end,
+    BackDeleteAt = function (self, inPosition_3f, inDimension_3f, inLineNo, inCharacterIndex)
+        
+    end,
     EraseAll = function(self)
         for i = 1, self.mMaxNumLines, 1 do
             self.mLineStringBuffers[i]:Update(vec3(0), vec3(0), " ")
         end
     end,
-    WrapWithinDimensions = function(self, inString, inStartingIndex, inLinePosition_3f, inDimension_3f)
+    GetCharacterPosition = function (self, inPosition_3f, inLineNo, inCharacterIndex)
+        local str = ComTable[self.mLineStringBuffers[inLineNo].mTextObjectId].mString
+        local substr = " "
+        if str then
+            substr = utf8.sub(str, 1, inCharacterIndex)
+        end
+        local dimens = self.mFontObject:GetDimension(substr)
+        return vec3(inPosition_3f.x + dimens.x, inPosition_3f.y + (inLineNo - 1) * self.mVerticalDrawSpacing, inPosition_3f.z)
+    end,
+    WrapWithinDimensions = function(self, inString, inStartingIndex, inLinePosition_3f, inDimension_3f, inLinePosInMultiline)
         local i = inStartingIndex
         local dimens = self.mFontObject:GetDimension(inString)
         if dimens.x > inDimension_3f.x then
             local sub = self.mFontObject:GetSubstringWithinDimension(inString, inDimension_3f.x)
-            self.mLineStringBuffers[i]:Update(inLinePosition_3f, inDimension_3f, sub.s)
+            self.mLineStringBuffers[i]:Update(inLinePosition_3f, inDimension_3f, sub.s, inLinePosInMultiline + sub.n)
             inLinePosition_3f.y = inLinePosition_3f.y + self.mVerticalDrawSpacing
             self:WrapWithinDimensions(utf8.sub(inString, sub.n, utf8.len(inString)), i + 1, inLinePosition_3f,
-                inDimension_3f)
+                inDimension_3f, inLinePosInMultiline + sub.n)
         else
-            self.mLineStringBuffers[i]:Update(inLinePosition_3f, inDimension_3f, inString)
+            self.mLineStringBuffers[i]:Update(inLinePosition_3f, inDimension_3f, inString, inLinePosInMultiline)
             inLinePosition_3f.y = inLinePosition_3f.y + self.mVerticalDrawSpacing
             i = i + 1
         end
@@ -101,8 +120,51 @@ Com.TextMultiLineObject = {
         linePosition.y = linePosition.y + self.mVerticalDrawSpacing -- The Text is drawn ABOVE the point, therefore a vertical-draw-spacing is being inserted
 
         local i = 1
+        local linepos = 1
         for Line in self.mStringBuffer:gmatch("(.-)\n") do
-            i = self:WrapWithinDimensions(Line, i, linePosition, inDimension_3f)
+            i = self:WrapWithinDimensions(Line, i, linePosition, inDimension_3f, linepos)
+            linepos = linepos + utf8.len(Line)
         end
+    end
+}
+
+-- could've inherited
+Com.TextMultiLineEditObject = {
+    mTextMultiLineObject = nil,
+    mCursor = nil,
+    mCursorPos_2u = nil,
+    mCursorWidth = nil,
+    New = function ( self, inPosition_3f, inDimension_3f, inFontObject, inMaxNoOfLines, inMaxChars, inVerticalDrawSpacing, inShouldWrap, inCursorWidth)
+        local Obj = {} 
+        setmetatable(Obj, self)
+        self.__index = self
+
+        Obj.mTextMultiLineObject = Com.TextMultiLineObject:New(inPosition_3f, inDimension_3f, inFontObject, inMaxNoOfLines, inMaxChars, inVerticalDrawSpacing, inShouldWrap)
+        Obj.mCursor = Com.TextCursorObject:New(inPosition_3f, vec3(0, 0, 1), vec4(1, 0, 0, 1))
+        if inCursorWidth then
+            Obj.mCursorWidth = inCursorWidth
+        else
+            Obj.mCursorWidth = 5
+        end
+
+        Obj:PutCursorAt(1, 1)
+        return Obj
+    end,
+    Insert = function (self, inCharacter)
+    end,
+    Delete = function (self)
+        
+    end,
+    SetCursor = function(self, inPosition_3f, inLine, inCharacterIndex, inCursorWidth)
+        local pos = self.mTextMultiLineObject:GetCharacterPosition(inPosition_3f, inLine, inCharacterIndex)
+        self.mCursorPos_2u = uvec2(inLine, inCharacterIndex)
+        self.mCursor:Update(pos, vec3(inCursorWidth, self.mTextMultiLineObject.mVerticalDrawSpacing, 1))
+    end,
+    PutCursorAt = function (self, inLine, inCharacterIndex)
+        self.mCursorPos_2u = uvec2(inLine, inCharacterIndex) 
+    end,
+    Update = function (self, inPosition_3f, inDimension_3f, inText)
+        self.mTextMultiLineObject:Update(inPosition_3f, inDimension_3f, inText)
+        self:SetCursor(inPosition_3f, self.mCursorPos_2u.x, self.mCursorPos_2u.y, self.mCursorWidth)
     end
 }
