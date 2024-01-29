562bd5cab93a98a2add72e91a1075230320c85f9
Bishal Kumar Jaiswal <077bct022.bishal@pcampus.edu.np>
2024-01-29 08:02:12 +0545

    grid layout

--- jkrgui/temp_bishal.lua
+++ jkrgui/temp_bishal.lua
@@ -1,25 +1,6 @@
 require "jkrgui.PrimitiveComponents"
 require "jkrgui.MaterialComponents"
 require "jkrgui.LayoutComponents"
-
-Com.TextButton = {
-    mTextButton = nil,
-    New = function(self, inPosition_3f, inDimension_3f, inFont, inString)
-        local Obj = Com.ButtonProxy:New(inPosition_3f, inDimension_3f)
-        setmetatable(self, Com.ButtonProxy) -- inherits Com.ButtonProxy
-        setmetatable(Obj, self)
-        self.__index = self
-        Obj.mText = inString
-        Obj.mTextButton = Com.TextButtonObject:New(inString, inFont, inPosition_3f, inDimension_3f)
-        return Obj
-    end,
-    Update = function(self, inPosition_3f, inDimension_3f, inString)
-        self.mText = inString
-        self.mTextButton:Update(inPosition_3f, inDimension_3f, inString)
-        Com.ButtonProxy.Update(self, inPosition_3f, inDimension_3f)
-    end
-}
-
 Com.FileMenuBarObject_Duplicate = {
     mMainArea = nil,
     mHeight = nil,
@@ -148,4 +129,36 @@ Com.ContextMenu_Duplicate = {
         end
     end,
 
+}
+Com.GridLayout = {
+    mPadding = nil,
+    mPosition_3f = nil,
+    mDimension_3f = nil,
+
+    New = function(self, inPadding)
+        local Obj = {
+            mPadding = inPadding
+        }
+        setmetatable(Obj, self)
+        self.__index = self
+
+        return Obj
+    end,
+    Update = function(self, inPosition_3f, inDimension_3f, inNoOfCell_Row, inNoOfCell_Coloumn, inComponentTable)
+        local position = vec3(inPosition_3f.x, inPosition_3f.y, inPosition_3f.z)
+        local dimension = vec3(inDimension_3f.x, inDimension_3f.y, inDimension_3f.z)
+        local OneCellDimension = vec3(0, 0, inDimension_3f.z)
+        local k = 1
+        OneCellDimension.x = (dimension.x - (self.mPadding * (inNoOfCell_Row - 1))) / inNoOfCell_Row
+        OneCellDimension.y = (dimension.y - (self.mPadding * (inNoOfCell_Coloumn - 1))) / inNoOfCell_Coloumn
+        for i = 1, inNoOfCell_Coloumn, 1 do
+            for j = 1, inNoOfCell_Row, 1 do
+                inComponentTable[k]:Update(position, OneCellDimension)
+                position.x = position.x + self.mPadding + OneCellDimension.x
+                k = k + 1
+            end
+            position.y = position.y + self.mPadding + OneCellDimension.y
+            position.x = inPosition_3f.x
+        end
+    end
 }
