2ae1dfd9fd5234ea7d890419857d8f1f5c1e4c42
Darshan Koirala <theanimatorspal@gmail.com>
2024-04-16 21:34:15 +0545

    GLTF Skin Loading

--- app.lua
+++ app.lua
@@ -13,7 +13,7 @@ local shape3d = Jkr.CreateShapeRenderer3D(i, w)
 local simple3d = Jkr.CreateSimple3DRenderer(i, w)
 local extended3d = Jkr.CreateSimple3DRenderer(i, w)
 local CubeGenerator = Jkr.Generator(Jkr.Shapes.Cube3D, vec3(1, 1, 1))
-shape3d:Add(CubeGenerator, vec3(0, 0, 0))
+--shape3d:Add(CubeGenerator, vec3(0, 0, 0))
 
 --local line = l:Add(vec3(100, 100, 1), vec3(500, 500, 1))
 local lGenerator = Jkr.Generator(Jkr.Shapes.RectangleFill, uvec2(50, 50))
@@ -58,12 +58,13 @@ jkrguiApp.AddBufferToUniform(Extended3dUniform, 1)
 Extended3dUniform:AddTexture(0, "res/models/CesiumLogoFlat.png")
 MT:Inject("__extended3dUniform__", Extended3dUniform)
 
+local SimpleSkinUniform = Jkr.Uniform3D(i, extended3d)
+
 MT:AddJobF(
    function()
-      local CubeModel = Jkr.glTF_Model("res/models/Duck.gltf")
+      --local CubeModel = Jkr.glTF_Model("res/models/Duck.gltf")
+      local CubeModel = Jkr.glTF_Model("res/models/SimpleSkin/SimpleSkin.gltf")
       local cubeId = __shape3d__:Add(CubeModel)
-      local duckModel = Jkr.glTF_Model("res/models/Duck.gltf")
-      local duckId = __shape3d__:Add(duckModel)
       __mt__:Inject("__cubeId__", cubeId)
    end
 )
@@ -72,11 +73,11 @@ local shit__I = 0
 function Update()
    MT:Wait()
    MT:Inject("I", shit__I)
-   local view = Jmath.LookAt(vec3(5, 5, 5), vec3(0, 0, 0), vec3(0, 1, 0)) -- view
+   local view = Jmath.LookAt(vec3(0, -5, 5), vec3(0, 0, 0), vec3(0, 1, 0)) -- view
    local projection = Jmath.Perspective(0.45, 1, 0.1, 100)
    local ubo = jkrguiApp.GetUBO(view, projection, vec3(5, 5, 5), vec4(math.sin(shit__I * 1000) * 5, 10, 5, 1))
    jkrguiApp.UpdateBufferToUniform(Extended3dUniform, 1, ubo)
-   shit__I = shit__I + 0.0001
+   -- shit__I = shit__I + 0.0001
 end
 
 function Draw()
@@ -101,14 +102,14 @@ function MTDraw()
          modelx = Jmath.Rotate_deg(modelx, I * 10000, vec3(1, 0, 0))
          __shape3d__:Bind(__w__, 0)
          __extended3d__:Bind(__w__, 0)
-         __extended3dUniform__:Bind(__w__, 0)
+         __extended3dUniform__:Bind(__w__, __extended3d__, 0)
          jkrguiApp.DrawBRDF(__extended3d__, __w__, __shape3d__, 0, modelx, vec3(1, 1, 1), vec3(1, 1, 0), 0)
 
          local modely = Jmath.GetIdentityMatrix4x4() -- model
          modely = Jmath.Scale(modely, vec3(0.01, 0.01, 0.01))
          modely = Jmath.Translate(modely, vec3(8, 0.4, 0.4))
          modely = Jmath.Rotate_deg(modely, I * 10000, vec3(1, 1, 1))
-         jkrguiApp.DrawBRDF(__extended3d__, __w__, __shape3d__, 2, modely, vec3(1, 1, 1), vec3(1, 1, 0), 0)
+         --jkrguiApp.DrawBRDF(__extended3d__, __w__, __shape3d__, 1, modely, vec3(1, 1, 1), vec3(1, 1, 0), 0)
          __w__:EndThreadCommandBuffer(0)
       end
    )
--- native/jkrguiApp.cpp
+++ native/jkrguiApp.cpp
@@ -1,6 +1,7 @@
+#include "Instance.hpp"
 #include "Renderers/Renderer_base.hpp"
 #include "Renderers/ThreeD/Shape3D.hpp"
-#include "ksai_config.hpp"
+#include "Renderers/ThreeD/glTF_Model.hpp"
 #include <Misc/ThreeD/Uniform3D.hpp>
 #include <Renderers/ThreeD/Simple3D.hpp>
 #include <WindowMulT.hpp>
@@ -147,56 +148,47 @@ const std::string_view BRDFFragment = R"FragmentShader(
 )FragmentShader";
 
 extern "C" DLLEXPORT int luaopen_jkrguiApp(lua_State* L) {
-          sol::state_view s(L);
-          auto jkrguiApp = s["jkrguiApp"].get_or_create<sol::table>();
-          jkrguiApp.set_function("hello", []() { std::cout << "Hello World from jkrguiApp\n"; });
-          jkrguiApp.set_function("GetBRDFVertexShader", [&]() { return BRDFVertex; });
-          jkrguiApp.set_function("GetBRDFFragmentShader", [&]() { return BRDFFragment; });
+               sol::state_view s(L);
+               auto jkrguiApp = s["jkrguiApp"].get_or_create<sol::table>();
+               jkrguiApp.set_function("hello", []() { std::cout << "Hello World from jkrguiApp\n"; });
+               jkrguiApp.set_function("GetBRDFVertexShader", [&]() { return BRDFVertex; });
+               jkrguiApp.set_function("GetBRDFFragmentShader", [&]() { return BRDFFragment; });
 
-          struct UBO {
-                    alignas(16) glm::mat4 view;
-                    alignas(16) glm::mat4 proj;
-                    glm::vec3 campos;
-                    alignas(16) glm::vec4 lights[10];
-          } ubo;
-          jkrguiApp.set_function("GetUBO", [](glm::mat4 inView, glm::mat4 inProj, glm::vec3 inCamPos, glm::vec4 inLightPos) {
-                    UBO ubo;
-                    ubo.view = inView;
-                    ubo.proj = inProj;
-                    ubo.campos = inCamPos;
-                    ubo.lights[0] = inLightPos;
-                    return ubo;
-          });
+               struct UBO {
+                              alignas(16) glm::mat4 view;
+                              alignas(16) glm::mat4 proj;
+                              glm::vec3 campos;
+                              alignas(16) glm::vec4 lights[10];
+               } ubo;
+               jkrguiApp.set_function("GetUBO", [](glm::mat4 inView, glm::mat4 inProj, glm::vec3 inCamPos, glm::vec4 inLightPos) {
+                              UBO ubo;
+                              ubo.view = inView;
+                              ubo.proj = inProj;
+                              ubo.campos = inCamPos;
+                              ubo.lights[0] = inLightPos;
+                              return ubo;
+               });
 
-          jkrguiApp.set_function("AddBufferToUniform",
-                                 [](Jkr::Misc::_3D::Uniform3D& inUniform, int inDstBinding) { inUniform.AddBuffer(inDstBinding, sizeof(UBO)); });
+               jkrguiApp.set_function("AddBufferToUniform", [](Jkr::Misc::_3D::Uniform3D& inUniform, int inDstBinding) { inUniform.AddUniformBuffer(inDstBinding, sizeof(UBO)); });
 
-          jkrguiApp.set_function("UpdateBufferToUniform", [](Jkr::Misc::_3D::Uniform3D& inUniform, int inDstBinding, UBO inubo) {
-                    inUniform.UpdateBuffer<UBO>(inDstBinding, inubo);
-          });
+               jkrguiApp.set_function("UpdateBufferToUniform", [](Jkr::Misc::_3D::Uniform3D& inUniform, int inDstBinding, UBO inubo) { inUniform.UpdateUniformBuffer<UBO>(inDstBinding, inubo); });
 
-          struct PushConstant {
-                    glm::mat4 mvp;
-                    glm::vec3 rough;
-                    glm::vec3 rgb;
-          };
-          jkrguiApp.set_function("DrawBRDF",
-                                 [](Jkr::Misc::_3D::Simple3D& inSimple3D,
-                                    Jkr::WindowMulT& inWindow,
-                                    Jkr::Renderer::_3D::Shape& inShape,
-                                    int inModelId,
-                                    glm::mat4 inModel,
-                                    glm::vec3 inRough,
-                                    glm::vec3 inRGB,
-                                    Jkr::Window::ParameterContext inParam) {
-                                           PushConstant p = {.mvp = inModel, .rough = inRough, .rgb = inRGB};
-                                           inSimple3D.Draw<PushConstant>(inWindow,
-                                                                         inShape,
-                                                                         p,
-                                                                         inShape.GetIndexOffsetAbsolute(inModelId),
-                                                                         inShape.GetIndexCount(inModelId),
-                                                                         1,
-                                                                         inParam);
-                                 });
-          return 1;
+               struct PushConstant {
+                              glm::mat4 mvp;
+                              glm::vec3 rough;
+                              glm::vec3 rgb;
+               };
+               jkrguiApp.set_function("DrawBRDF",
+                                      [](Jkr::Misc::_3D::Simple3D& inSimple3D,
+                                         Jkr::WindowMulT& inWindow,
+                                         Jkr::Renderer::_3D::Shape& inShape,
+                                         int inModelId,
+                                         glm::mat4 inModel,
+                                         glm::vec3 inRough,
+                                         glm::vec3 inRGB,
+                                         Jkr::Window::ParameterContext inParam) {
+                                                     PushConstant p = {.mvp = inModel, .rough = inRough, .rgb = inRGB};
+                                                     inSimple3D.Draw<PushConstant>(inWindow, inShape, p, inShape.GetIndexOffsetAbsolute(inModelId), inShape.GetIndexCount(inModelId), 1, inParam);
+                                      });
+               return 1;
 }
