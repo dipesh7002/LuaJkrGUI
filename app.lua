dad8095c41327cc6ab3a6382a1a114117e0f2d53
Darshan Koirala <theanimatorspal@gmail.com>
2024-04-20 13:00:13 +0545

    CustomImmage Painter Not WOrking

--- app.lua
+++ app.lua
@@ -1,156 +1,45 @@
-require "JkrGUIv2.JkrGUIv2"
-ImportShared("jkrguiApp")
+require "JkrGUIv2.Basic"
+require "JkrGUIv2.Widgets"
 
-local i = Jkr.CreateInstance(nil, nil, 4)
-local MT = Jkr.MultiThreading(i)
-local w = Jkr.CreateWindow(i, "Hello", vec2(500, 500))
-local e = Jkr.CreateEventManager()
---local l = Jkr.CreateLineRenderer(i, w)
+local SampraInstance = Jkr.CreateInstance()
+local SampraWindow = Jkr.CreateWindow(SampraInstance, "Samprahar Returns", vec2(600, 300))
+local SampraEvent = Jkr.CreateEventManager()
+local SampraWidget = Jkr.CreateWidgetRenderer(SampraInstance, SampraWindow)
+local Font = SampraWidget.CreateFont("res/fonts/font.ttf", 16)
+local TextLabel = SampraWidget.CreateTextLabel(vec3(200, 200, 20), vec3(0), Font, "Hellow", vec4(1, 1, 1, 1))
 
-local shape = Jkr.CreateShapeRenderer(i, w)
-local TR = Jkr.CreateTextRendererBestTextAlt(i, shape)
-local shape3d = Jkr.CreateShapeRenderer3D(i, w)
-local simple3d = Jkr.CreateSimple3DRenderer(i, w)
-local extended3d = Jkr.CreateSimple3DRenderer(i, w)
-local skinned3d = Jkr.CreateSimple3DRenderer(i, w)
-local CubeGenerator = Jkr.Generator(Jkr.Shapes.Cube3D, vec3(1, 1, 1))
---shape3d:Add(CubeGenerator, vec3(0, 0, 0))
+local CustomImage = Jkr.CreateCustomPainterImage(SampraInstance, SampraWindow, 100, 100)
+local CustomPainter = Jkr.CreateCustomImagePainter("res/cache/CustomPainter.glsl",
+   Jkr.GetDefaultResource("CustomImagePainter", "RoundedRectangle"))
+CustomPainter:Store(SampraInstance, SampraWindow)
+CustomImage:Register(SampraInstance, CustomPainter.handle) -- TODO Wrap This
 
---local line = l:Add(vec3(100, 100, 1), vec3(500, 500, 1))
-local lGenerator = Jkr.Generator(Jkr.Shapes.RectangleFill, uvec2(50, 50))
-local id = shape:Add(lGenerator, vec3(10, 10, 20))
-local font = TR:AddFontFace("res/fonts/font.ttf", 20)
-local font_small = TR:AddFontFace("res/fonts/font.ttf", 15)
-local text = TR:Add(font, vec3(100, 100, 5), "जय श्री राम")
+local Rectangle = Jkr.Generator(Jkr.Shapes.RectangleFill, uvec2(50, 50))
+local ShapeRenderer = Jkr.CreateShapeRenderer(SampraInstance, SampraWindow)
+local RectangleId = ShapeRenderer:Add(Rectangle, vec3(50, 50, 50))
+local Image = ShapeRenderer:AddImage(100, 100)
 
-ConfigureMultiThreading(MT)
-Jkr.MultiThreadingInject(
-   MT,
-   {
-      { "_i_",          i },
-      { "_w_",          w },
-      { "_shape3d_",    shape3d },
-      { "_simple3d_",   simple3d },
-      { "_extended3d_", extended3d },
-      { "_skinned3d_",  skinned3d },
-      { "_mt_",         MT },
-   }
-)
-
-MT:InjectScriptF(
-   function()
-      ImportShared("jkrguiApp")
-   end
-)
-
-
-
-function CompileBRDFShader()
-   local vshader = jkrguiApp.GetBRDFVertexShader()
-   local fshader = jkrguiApp.GetBRDFFragmentShader()
-   extended3d:Compile(
-      i,
-      w,
-      "cache2/extended3D.glsl",
-      vshader,
-      fshader,
-      GetDefaultResource("Simple3D", "Compute"),
-      false
-   )
+local Draw = function()
+   SampraWidget.Draw()
 end
 
-CompileBRDFShader()
-
-
-
-local Extended3dUniform = Jkr.Uniform3D(i, extended3d)
-jkrguiApp.AddBufferToUniform(Extended3dUniform, 1)
-Extended3dUniform:AddTexture(0, "res/models/CesiumLogoFlat.png")
-MT:Inject("_extended3dUniform_", Extended3dUniform)
-
-local SimpleSkinUniform = Jkr.Uniform3D(i, extended3d)
-local SimpleSkinModel = Jkr.glTF_Model("res/models/SimpleSkin/SimpleSkin.gltf")
-local simpleSkin = shape3d:Add(SimpleSkinModel)
-Jkr.GetGLTFInfo(SimpleSkinModel, true)
-
-
-function CompileSkinnedShader()
-   skinned3d:Compile(
-      i,
-      w,
-      "res/models/SimpleSkin/VFC.glsl",
-      Jkr.GetGLTFVertexShader(SimpleSkinModel),
-      GetDefaultResource("Simple3D", "Fragment"),
-      GetDefaultResource("Simple3D", "Compute"),
-      false
-   )
+local Update = function()
+   SampraWidget.Event()
+   SampraWidget.Update()
 end
 
-CompileSkinnedShader()
-local skinnedShaderUniform3D = Jkr.Uniform3D(i, skinned3d, SimpleSkinModel, 0, true)
-MT:Inject("_skinnedShaderUniform3D_", skinnedShaderUniform3D)
+local Dispatch = function()
+   CustomPainter:Bind(SampraWindow, Jkr.CmdParam.None)
+   CustomPainter:BindImageFromImage(SampraWindow, CustomImage, Jkr.CmdParam.None)
+   local pc = Jkr.DefaultCustomImagePainterPushConstant()
+   pc.x = vec4(10, 5, 5, 5)
+   pc.y = vec4(1, 0, 0, 1)
+   pc.z = vec4(0.8, 5, 5, 5)
+   --CustomPainter:Draw(SampraWindow, pc, 50, 50, 1, Jkr.CmdParam.None)
+   ShapeRenderer:CopyToImage(Image, CustomImage)
+   ShapeRenderer:Dispatch(SampraWindow)
 
-MT:AddJobF(
-   function()
-      --local CubeModel = Jkr.glTF_Model("res/models/Duck.gltf")
-      local CubeModel = Jkr.glTF_Model("res/models/SimpleSkin/SimpleSkin.gltf")
-      local cubeId = _shape3d_:Add(CubeModel)
-      _mt_:Inject("_cubeId_", cubeId)
-   end
-)
-
-local shit_I = 0
-function Update()
-   MT:Wait()
-   MT:Inject("I", shit_I)
-   local view = Jmath.LookAt(vec3(0, -5, 5), vec3(0, 0, 0), vec3(0, 1, 0)) -- view
-   local projection = Jmath.Perspective(0.45, 1, 0.1, 100)
-   local ubo = jkrguiApp.GetUBO(Jmath.GetIdentityMatrix4x4(), Jmath.GetIdentityMatrix4x4(), vec3(5, 5, 5),
-      vec4(math.sin(shit_I * 1000) * 5, 10, 5, 1))
-   jkrguiApp.UpdateBufferToUniform(Extended3dUniform, 1, ubo)
-   skinnedShaderUniform3D:UpdateByGLTFAnimation(SimpleSkinModel, shit_I, 0)
-   shit_I = shit_I + 0.0001
+   SampraWidget.Dispatch()
 end
 
-function Draw()
-end
-
-function Dispatch()
-   --   l:Dispatch(w)
-   shape:Dispatch(w)
-end
-
-function PostProcess()
-end
-
-function MTDraw()
-   MT:AddJobF(
-      function()
-         _w_:BeginThreadCommandBuffer(0)
-         _w_:SetDefaultViewport(0)
-         _w_:SetDefaultScissor(0)
-         local modelx = Jmath.GetIdentityMatrix4x4() -- model
-         modelx = Jmath.Scale(modelx, vec3(1, 1, 1))
-         modelx = Jmath.Rotate_deg(modelx, I * 10000, vec3(1, 0, 0))
-         -- _extended3d_:Bind(_w_, 0)
-         -- _extended3dUniform_:Bind(_w_, _extended3d_, 0)
-         -- jkrguiApp.DrawBRDF(_extended3d_, _w_, _shape3d_, 0, modelx, vec3(1, 1, 1), vec3(1, 1, 0), 0)
-         _shape3d_:Bind(_w_, 0)
-         _skinned3d_:Bind(_w_, 0)
-         _skinnedShaderUniform3D_:Bind(_w_, _skinned3d_, 0)
-         local view       = Jmath.LookAt(vec3(0, -5, 5), vec3(0, 0, 0), vec3(0, 1, 0)) -- view
-         local projection = Jmath.Perspective(0.45, 1, 0.1, 100)
-         local model      = Jmath.GetIdentityMatrix4x4()
-         jkrguiApp.DrawSkinned(_w_, _shape3d_, _skinned3d_, projection * view * model, Jmath.GetIdentityMatrix4x4(), 0, 0)
-
-
-         _w_:EndThreadCommandBuffer(0)
-      end
-   )
-end
-
-function MTExecute()
-   w:ExecuteThreadCommandBuffer(0)
-end
-
-Jkr.DebugMainLoop(w, e, Update, Dispatch, Draw, PostProcess, nil, MT, MTDraw, MTExecute)
+Jkr.DebugMainLoop(SampraWindow, SampraEvent, Update, Dispatch, Draw, nil, vec4(0, 0, 0, 1))
